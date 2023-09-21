f14ca8160ea0a284680811091b92f1a53e4ca7fd
majdishomali <majdi.shomali.1997@gmail.com>
2023-09-21 16:52:02 -0700

    edit 3

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,33 +1,33 @@
 import AddUserForm from "@/components/AddUserForm";
 import UserCard from "@/components/UserCard";
 const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
-// const getUsers = async () => {
-//     try {
-//       const res = await fetch(`${NEXT_PUBLIC_API_URL}/users`, {
-//         // cache:"force-cache",//SSG getStaticSideProps
-//         cache: "no-store", //SSR getServerSideProps
-//         // next: {
-//         //   revalidate: 20, //ISR===== ssr with sec
-//         // },
-//       });
+const getUsers = async () => {
+    try {
+      const res = await fetch(`${NEXT_PUBLIC_API_URL}/users`, {
+        // cache:"force-cache",//SSG getStaticSideProps
+        cache: "no-store", //SSR getServerSideProps
+        // next: {
+        //   revalidate: 20, //ISR===== ssr with sec
+        // },
+      });
   
-//       if (!res.ok) {
-//         throw new Error("Failed to fetch users");
-//       }
+      if (!res.ok) {
+        throw new Error("Failed to fetch users");
+      }
   
-//       return res.json();
-//     } catch (error) {
-//       console.log("Error loading topics: ", error);
-//     }
-//   };
+      return res.json();
+    } catch (error) {
+      console.log("Error loading topics: ", error);
+    }
+  };
   
 const UsersPage = async() => {
-    // const { users } = await getUsers();
+    const { users } = await getUsers();
     return (
       <>
         <AddUserForm />
         <div className="flex flex-wrap justify-center gap-5 my-5">
-          {/* <UserCard users={users} /> */}
+          <UserCard users={users} />
         </div>
       </>
     );
