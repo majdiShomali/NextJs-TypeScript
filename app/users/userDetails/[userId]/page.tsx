3d74b877b682ab15e9c672e9e3d31909da11221a
majdishomali <majdi.shomali.1997@gmail.com>
2023-09-21 17:44:47 -0700

    edt

--- app/users/userDetails/[userId]/page.tsx
+++ app/users/userDetails/[userId]/page.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import UserCard from '@/components/UserCard'
 const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// async function getUser(userId:string){
-//   const topicResponce =await fetch(`${NEXT_PUBLIC_API_URL}/api/users/${userId}`,{cache:"force-cache"})
-//   return topicResponce.json();
-// }
+async function getUser(userId:string){
+  const topicResponce =await fetch(`${NEXT_PUBLIC_API_URL}/api/users/${userId}`,{cache:"force-cache"})
+  return topicResponce.json();
+}
 
 interface UserDetailsProps {
   params: {
@@ -13,11 +13,11 @@ interface UserDetailsProps {
   };
 }
 const UserDetails: React.FC<UserDetailsProps>  = async ({params}) => {
-  // const {user} = await getUser(params.userId)
+  const {user} = await getUser(params.userId)
 
   return (
     <div className="w-full flex flex-wrap justify-center">
-    {/* <UserCard users={[user]} /> */}
+    <UserCard users={[user]} />
 </div>
   )
 }
